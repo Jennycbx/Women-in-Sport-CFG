45e384caac2e4c19c31201c86ec0bc8b6e122bc5
kkinni <katekinninmont@gmail.com>
2022-08-02 00:00:56 +0100

    players update

---
+++ players/players.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("jf-modal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementById("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
