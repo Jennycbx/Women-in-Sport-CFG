eaceb39e09a1cbd9569dcef79ba3fee7593a75d8
kkinni <katekinninmont@gmail.com>
2022-08-02 19:14:38 +0100

    players final changes

--- players/players.js
+++ players/players.js
@@ -1,6 +1,6 @@
 var modal = document.getElementById("jf-modal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementById("close")[0];
+var btn = document.getElementById("jfBtn");
+var span = document.getElementById("jfClose");
 
 btn.onclick = function () {
   modal.style.display = "block";
@@ -8,6 +8,24 @@ btn.onclick = function () {
 
 span.onclick = function () {
   modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var modal2 = document.getElementById("bs-modal");
+var btn = document.getElementById("bsBtn");
+var span = document.getElementById("bsClose");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal2.style.display = "none";
 };
 
 window.onclick = function (event) {
