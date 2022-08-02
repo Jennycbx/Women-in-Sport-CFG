50bf9b42651c42f6d08c99328f8c1864331434b4
kkinni <katekinninmont@gmail.com>
2022-08-02 19:50:30 +0100

    Update players.js

--- players/players.js
+++ players/players.js
@@ -21,11 +21,11 @@ var btn = document.getElementById("bsBtn");
 var span = document.getElementById("bsClose");
 
 btn.onclick = function () {
-  modal.style.display = "block";
+  modal2.style.display = "block";
 };
 
 span.onclick = function () {
-  modal2.style.display = "none";
+  modal.style.display = "none";
 };
 
 window.onclick = function (event) {
